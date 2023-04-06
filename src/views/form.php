f6e08090e550d14e6e1e966bfe2f12f4c1e18218
Bokassa jean le Grand <jean-le-grand.bokassa@wildcodeschool.com>
2023-04-06 06:29:38 +0200

    marmiwild part I

---
+++ src/views/form.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>List of Recipes</title>
+</head>
+<body>
+<h1>Add Recipe</h1>
+<?php if (!empty($errors)): ?>
+    <?php foreach ($errors as $error): ?>
+        <li><?= $error; ?></li>
+    <?php endforeach; ?>
+<?php endif; ?>
+<form method="post">
+    <div>
+        <label for="title">Title</label>
+        <input type="text" name="title" id="title">
+    </div>
+    <br>
+    <div>
+        <label for="description">Description</label><br>
+        <textarea name="description" id="description" cols="30" rows="10"></textarea>
+    </div>
+    <button>Submit</button>
+</form>
+</body>
+</html>
